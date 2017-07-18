4e3b2435e778611a114b52060fc091d24a3150f4
enble du <384509085@qq.com>
2017-07-18 21:57:23 +0800

    ViewPager

--- ToDoList/app/src/main/java/enbledu/todolist/activity/MainActivity.java
+++ ToDoList/app/src/main/java/enbledu/todolist/activity/MainActivity.java
@@ -1,15 +1,41 @@
 package enbledu.todolist.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import enbledu.todolist.R;
+import enbledu.todolist.adapter.MyFragmentAdapter;
+import enbledu.todolist.fragment.FragmentAlarm;
+import enbledu.todolist.fragment.FragmentTodoList;
+import enbledu.todolist.helper.DepthPageTransformer;
 
 public class MainActivity extends AppCompatActivity {
-
+    ViewPager mvViewPager;
+    private Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+        Fragment fragment1 = new FragmentTodoList(mContext);
+        Fragment fragment2 = new FragmentAlarm(mContext);
+        //Fragment fragment4 = new Fragment();
+
+        // 将要分页显示的View装入数组中
+        List<Fragment> list = new ArrayList<Fragment>();
+        list.add(fragment1);
+        list.add(fragment2);
+        mvViewPager=(ViewPager)findViewById(R.id.pager);
+        mvViewPager.setAdapter(new MyFragmentAdapter(getSupportFragmentManager(), list));
+        mvViewPager.setPageTransformer(true, new DepthPageTransformer());
+        mvViewPager.setCurrentItem(0);
+        mvViewPager.setOffscreenPageLimit(5);
+
     }
 }
---
+++ ToDoList/app/src/main/java/enbledu/todolist/fragment/FragmentAlarm.java
@@ -0,0 +1,16 @@
+package enbledu.todolist.fragment;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2017/7/18 0018.
+ */
+
+public class FragmentAlarm extends Fragment {
+    private Context mContext;
+
+    public FragmentAlarm(Context mContext) {
+        this.mContext = mContext;
+    }
+}
