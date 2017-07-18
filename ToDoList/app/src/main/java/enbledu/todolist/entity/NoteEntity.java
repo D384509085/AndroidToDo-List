82ebbfd88981fd83d6d3cd35b02be58cf5870d85
enble du <384509085@qq.com>
2017-07-18 10:55:04 +0800

    entity

---
+++ ToDoList/app/src/main/java/enbledu/todolist/entity/NoteEntity.java
@@ -0,0 +1,14 @@
+package enbledu.todolist.entity;
+
+/**
+ * Created by Administrator on 2017/7/18 0018.
+ */
+
+public class NoteEntity {
+    private String title;
+    private String context;
+    private String createTime;
+    private String stopTime;
+    private int priorty;
+    private boolean isFinished;
+}
