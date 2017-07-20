be77ee76485eb54bfbfe28c642e04a89db6121dd
enble du <384509085@qq.com>
2017-07-20 22:39:02 +0800

    edit

--- ToDoList/app/src/main/java/enbledu/todolist/activity/EditActivity.java
+++ ToDoList/app/src/main/java/enbledu/todolist/activity/EditActivity.java
@@ -5,7 +5,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.TimePicker;
 
 import enbledu.todolist.R;
 
@@ -13,7 +17,12 @@ public class EditActivity extends AppCompatActivity {
 
     private String TAG = "EditActivity";
     private NumberPicker mNumberPicker;
-    Toolbar editToolbar;
+    private CalendarView mCalendarView;
+    private TimePicker mTimePicker;
+    private EditText titleEdit;
+    private EditText contextEdit;
+    private Button okButton;
+    private Toolbar editToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +31,28 @@ public class EditActivity extends AppCompatActivity {
         Log.d(TAG, String.valueOf(getIntent() == null));
         initToolbar();
         initPicker();
+        init();
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text, title;
+                text = contextEdit.getText().toString();
+                title = titleEdit.getText().toString();
+                 
+            }
+        });
+    }
+
+    private void init() {
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        mTimePicker = (TimePicker) findViewById(R.id.timePicker);
+        titleEdit = (EditText) findViewById(R.id.edit_title);
+        contextEdit = (EditText) findViewById(R.id.edit_context);
+        okButton = (Button) findViewById(R.id.edit_ok_button);
     }
 
     private void initPicker() {
+
         mNumberPicker = (NumberPicker) findViewById(R.id.show_num_picker);
        // mNumberPicker.setFormatter(this);
        // mNumberPicker.setOnValueChangedListener(this);
